80c9d118b8943bb890e49a3b96ac60809b1a5310
Jungmo Kang <kangjmo91@gmail.com>
2020-05-29 16:35:12 +0000

    if와else로 db에 word가 있으면 거기서 가져오게끔

--- main.py
+++ main.py
@@ -14,8 +14,12 @@ def report():
   word = request.args.get('word')
   if word:
     word = word.lower() #대문자 입력해도 소문자로 전환해줌
-    jobs = get_jobs(word)
-    db[word] =jobs
+    fromDb = db.get(word)
+    if fromDb:
+      jobs = fromDb
+    else:
+      jobs = get_jobs(word)
+      db[word] = jobs
 
   else:   #문자 입력이 안된다면 처음으로 돌아가게끔
     return redirect("/")  #redirect를 import 해줘야 한다 잊지말자
