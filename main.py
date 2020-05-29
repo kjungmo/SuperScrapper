bb4c3a0d644124884c6337f265e1d65761730622
Jungmo Kang <kangjmo91@gmail.com>
2020-05-29 16:32:45 +0000

    db를 추가

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ def report():
   if word:
     word = word.lower() #대문자 입력해도 소문자로 전환해줌
     jobs = get_jobs(word)
-    print(jobs)
+    db[word] =jobs
+
   else:   #문자 입력이 안된다면 처음으로 돌아가게끔
     return redirect("/")  #redirect를 import 해줘야 한다 잊지말자
   return render_template("report.html", searchingBy=word)
