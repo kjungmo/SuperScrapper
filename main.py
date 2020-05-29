70cf0302c7f98197c2d36fee99fbba19d6c1866a
Jungmo Kang <kangjmo91@gmail.com>
2020-05-29 17:17:38 +0000

    grid-template-columns 에서 1fr이 오타가 났다 (1f4로 썼었다) 그로인해 title/company/location/link 표가 형성 되지 않았다

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ app = Flask("SuperScrapper")
 
 db = {}
 
-
 @app.route("/")
 def home():
     return render_template("potato.html")
@@ -13,20 +12,20 @@ def home():
 
 @app.route("/report")
 def report():
-    word = request.args.get('word')
-    if word:
-        word = word.lower()  #대문자 입력해도 소문자로 전환해줌
-        existingjobs = db.get(word)
-        if existingjobs:
-            jobs = existingjobs
-        else:
-            jobs = get_jobs(word)
-            db[word] = jobs
+  word = request.args.get('word')
+  if word:
+    word = word.lower()  #대문자 입력해도 소문자로 전환해줌
+    existingjobs = db.get(word)
+    if existingjobs:
+      jobs = existingjobs
+    else:
+      jobs = get_jobs(word)
+      db[word] = jobs
 
-    else:  #문자 입력이 안된다면 처음으로 돌아가게끔
-        return redirect("/")  #redirect를 import 해줘야 한다 잊지말자
-    return render_template(
-        "report.html", searchingBy=word, resultsNumber=len(jobs), jobs=jobs)
+  else:  #문자 입력이 안된다면 처음으로 돌아가게끔
+    return redirect("/")  #redirect를 import 해줘야 한다 잊지말자
+  return render_template(
+    "report.html", searchingBy=word, resultsNumber=len(jobs), jobs=jobs)
 
 
 app.run(host="0.0.0.0")
